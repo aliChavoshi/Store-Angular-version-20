d392a06739cbdd3e9e7979d05b9df5c43d0673d6
Ali Chavoshi <alichavoshii1372@gmail.com>
2025-09-30 15:52:30 +0330

    add some new components

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { SideNavigation } from "./side-navigation/side-navigation";
 import { Products } from "../products/products";
+import { Pagination } from "./pagination/pagination";
+import { Sorting } from "./sorting/sorting";
 
 @Component({
   selector: 'app-home',
-  imports: [SideNavigation, Products],
+  imports: [SideNavigation, Products, Pagination, Sorting],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
---
+++ src/app/components/home/sorting/sorting.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sorting',
+  imports: [],
+  templateUrl: './sorting.html',
+  styleUrl: './sorting.css'
+})
+export class Sorting {
+
+}
