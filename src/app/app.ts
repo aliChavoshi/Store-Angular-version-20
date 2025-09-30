5ce16595fcc26579dc5dadf71a1fbae57d7699dc
Ali Chavoshi <alichavoshii1372@gmail.com>
2025-09-30 14:24:04 +0330

    add home component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { Header } from "./components/header/header";
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.css',
-  imports: [Header]
+  imports: [Header, RouterOutlet]
 })
 export class App {
   protected readonly title = signal('Store-Angular');
