e99a49de3cf2425d21a2df6701350c1dbc0050ac
Ali Chavoshi <alichavoshii1372@gmail.com>
2025-10-08 16:56:14 +0330

    get product

--- src/app/components/products/product-list/product-item/product-item.ts
+++ src/app/components/products/product-list/product-item/product-item.ts
@@ -1,10 +1,11 @@
 import { Component, input } from '@angular/core';
 import { Product } from '../../../../models/product';
 import { DecimalPipe } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-product-item',
-  imports: [DecimalPipe],
+  imports: [DecimalPipe, RouterLink],
   templateUrl: './product-item.html',
   styleUrl: './product-item.css'
 })
--- src/app/components/products/product-single/product-single.component.ts
+++ src/app/components/products/product-single/product-single.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProductService } from '../product.service';
+import { Product } from '../../../models/product';
 
 @Component({
   selector: 'app-product-single',
@@ -6,10 +9,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-single.component.css']
 })
 export class ProductSingleComponent implements OnInit {
+  product!: Product;
+  private route = inject(ActivatedRoute);
+  private productService = inject(ProductService);
 
-  constructor() { }
-
-  ngOnInit() {
+  id!: number;
+  constructor() {
+    this.route.params.subscribe(res => {
+      this.id = res['id'];
+    });
   }
 
+  ngOnInit() {
+    this.productService.getProduct(this.id).subscribe(res => {
+      this.product = res;
+    });
+  }
 }
